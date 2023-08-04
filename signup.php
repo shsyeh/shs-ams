e5382b32df1d5fb6043573fd50d6ba164fc876b1
WY <byoink@gmail.com>
2023-08-03 17:43:20 -0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,25 +7,25 @@ include('connect.php');
       if(isset($_POST['signup'])){
 
         if(empty($_POST['email'])){
-          throw new Exception("Email cann't be empty.");
+          throw new Exception("Email can't be empty.");
         }
 
         if(empty($_POST['uname'])){
-           throw new Exception("Username cann't be empty.");
+           throw new Exception("Username can't be empty.");
         }
 
         if(empty($_POST['pass'])){
-           throw new Exception("Password cann't be empty.");
+           throw new Exception("Password can't be empty.");
         }
         
         if(empty($_POST['fname'])){
-           throw new Exception("Username cann't be empty.");
+           throw new Exception("Username can't be empty.");
         }
         if(empty($_POST['phone'])){
-           throw new Exception("Username cann't be empty.");
+           throw new Exception("Username can't be empty.");
         }
         if(empty($_POST['type'])){
-           throw new Exception("Username cann't be empty.");
+           throw new Exception("Username can't be empty.");
         }
 
         $result = mysql_query("insert into admininfo(username,password,email,fname,phone,type) values('$_POST[uname]','$_POST[pass]','$_POST[email]','$_POST[fname]','$_POST[phone]','$_POST[type]')");
@@ -90,7 +90,7 @@ include('connect.php');
         </tr>
         <tr>
           <td>Password</td>
-          <td><input type="Password" name="pass"></td>
+          <td><input type="text" name="pass"></td>
         </tr>
 
         <tr>
