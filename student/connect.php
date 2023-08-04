a7fa9cf69773361192bc0d572a664bf32e509409
WY <byoink@gmail.com>
2023-08-03 17:28:29 -0700

    Adding db credentials
    Not worried about security

--- student/connect.php
+++ student/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-mysql_connect('localhost','root','') or die('Cannot connect to server');
+mysql_connect('localhost','skylinedb','physics402021') or die('Cannot connect to server');
 mysql_select_db('attsystem') or die ('Cannot found database');
 
 ?>
